17f797bbddf6cee4b0be91db06250968df524e12
AreSvel9 <129761204+AreSvel9@users.noreply.github.com>
2023-08-01 18:38:07 +0300

    Add files via upload

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import AddPostForm
+from .models import Post
+from .filters import PostFilter
+
+
+class PostsList(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+    ordering = '-dateCreation'
+    paginate_by = 10
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'post_search.html'
+    context_object_name = 'posts'
+    ordering = ['-dateCreation']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class PostCreate(CreateView):
+    template_name = 'post_create.html'
+    form_class = AddPostForm
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    template_name = 'post_create.html'
+    form_class = AddPostForm
+
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+
+class PostDelete(DeleteView):
+    template_name = 'post_delete.html'
+    queryset = Post.objects.all()
+    success_url = reverse_lazy('news')
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'protect/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_premium'] = not self.request.user.groups.filter(name='premium').exists()
+        return context
+
